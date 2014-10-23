6d96a8ab9daf05111be4fb26e9233e2defab79c8
Ty <ty.clark@bayefficiency.com>
2014-10-22 17:43:00 -0700

    changes on phone call with Brian

--- app/calculation/utility_calc.rb
+++ app/calculation/utility_calc.rb
@@ -1,21 +1,40 @@
 #Utility Calculation
 
 
-class UtilityCalc 
+class UtilityCalc
+  #def initialize(equipment)
+  #  @equipment = equipment
+  #end 
+
   def schedule_calculation ()
   #Schedule.create ([{:name => '24x7 number 3', :w_day_begin => '2000-01-01 00:00:01', :w_day_stop => '2000-01-01 11:59:59', :w_end_begin => '2000-01-01 00:00:01', :w_end_stop => '2000-01-01 11:59:59'}])
     Schedule.first
   end
 
   def weekday_utility_cost
-  	equipment = Equipment.find(1)
+  	occupant = Occupant.find(1)
+    equipment = Equipment.find(3)
   	equipment.schedule.weekday_hours * 0.15
   end
 
   def weekday_cost
-  	schedule = Schedule.new(w_day_stop:Time.now, w_day_begin:(Time.now-2.days))
-  	schedule.weekday_hours
+    schedule = @equipment.schedule
+    daily_hours = schdule.w_day_stop - schedule.w_day_start
+    weekly_hours = daily_hours * 5
+
+    @equipment.zone.occupant.utility.summer_energy_peak * weekly_hours
+
+   #  #Equipment.find_by(name: 'name!') # only returns the first one
+   #  #Equipment.where(name: 'name') # returns a collection of all
+  	# schedule = Schedule.new(w_day_stop:Time.now, w_day_begin:(Time.now-2.days))
+  	# schedule.weekday_hours
   end
 
 
 end
+
+
+
+# in yoru console
+#equipment = Equipment.first # or whatever
+#UtilityCalc.new(equipment)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Schedule.delete_all
+Equipment.delete_all
+schedule = Schedule.create!(w_day_stop:Time.now, w_day_begin:(Time.now-2.days))
+equipment = Equipment.create!(zone_id:1, quantity:3, years_installed:5)
+puts 'succcess'
