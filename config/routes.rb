83337a43926731cd4a49f76985933fd8c2af7258
Ty <ty.clark@bayefficiency.com>
2014-11-04 08:14:52 -0800

    adding route for google verification

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ end
  root 'welcome#index'
  get '/sfenvironment', to: redirect('/#')
  get '/benchmarking', to: 'welcome#benchmarking'
+ get '/google20f2c5dc8a027e79.html', to: 'welcome#google20f2c5dc8a027e79.html'
 end
 
 
