ed30e2b88a55693440d2fa12a02cc1c17f97fa75
Ty <ty.clark@bayefficiency.com>
2014-11-08 11:39:24 -0800

    updated RelampCalc with new reverse polymorphic associations

--- app/calculation/relamp_calc.rb
+++ app/calculation/relamp_calc.rb
@@ -11,15 +11,16 @@ class RelampCalc
     part = @equipment.part
     schedule = @equipment.schedule
 
-    if part.part_info_type == 2
+    if part.element_type == "Hvac"
       total_hours = 8760
     else
-      if part.light.occ_sensor_ysn
-        powered_pct = 1- part.light.occ_sensor.savings_pct
+      if part.element.occ_sensor_ysn
+        powered_pct = 1- part.element.occ_sensor.savings_pct
       else
         powered_pct = 1
       end
       
+
       if schedule.w_day_stop > schedule.w_day_begin
       #If it is a regular schedule (start time is before stop time.)
         schedule_diff = ((schedule.w_day_stop-schedule.w_day_begin)*(5 * 52.1775/3600))+((schedule.w_end_stop-schedule.w_end_begin)*(2 * 52.1775/3600))
